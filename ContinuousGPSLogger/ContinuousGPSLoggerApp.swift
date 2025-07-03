6e2570e0e49dbe438bb61af5797e1a1865ff2c22
sakuyacatcat <shingo.takagi0116@gmail.com>
2025-07-03 19:59:49 +0900

    first commit

---
+++ ContinuousGPSLogger/ContinuousGPSLoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuousGPSLoggerApp.swift
+//  ContinuousGPSLogger
+//
+//  Created by Shingo Takagi on 2025/07/03.
+//
+
+import SwiftUI
+
+@main
+struct ContinuousGPSLoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContinuousGPSLoggerTests/ContinuousGPSLoggerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuousGPSLoggerTests.swift
+//  ContinuousGPSLoggerTests
+//
+//  Created by Shingo Takagi on 2025/07/03.
+//
+
+import Testing
+@testable import ContinuousGPSLogger
+
+struct ContinuousGPSLoggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
